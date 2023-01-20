e01f245bd3c76d541e4e553bb34002158dcf3876
HR <raohritik30@gmail.com>
2023-01-20 15:49:39 +0530

    first commit

---
+++ routes/account.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const accCtrl = require('../controllers/account-controller');
+
+module.exports = function (passport) {
+    router.get("/", function (req, res, next) {
+        res.redirect('account/login');
+    });
+
+    router.get("/login", accCtrl.login_get);
+
+    router.post("/login", accCtrl.login_post(passport));
+
+    return router;
+};
